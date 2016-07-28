b20783e955791a6d15dbce34ccdedf2299b98838
johnngugi <johnngugi777@gmail.com>
2016-07-28 21:34:47 +0300

    Add data search exercise

---
+++ data-search/exercise.py
@@ -0,0 +1,14 @@
+import csv
+with open ("MOCK_DATA.csv", "r") as csvfile:
+    idnumber = raw_input("Please ente your ID: ")
+    read_data = csv.reader(csvfile, delimiter=',')
+    for row in read_data:
+        if idnumber == row[0]:
+            print row[1] + ' '+ row[3] + ' '+ row[5]
+            break
+        else:
+            pass
+
+
+
+
