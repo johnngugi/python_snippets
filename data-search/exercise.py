099490e62b8050ee0244739d27a860594ced9239
johnngugi <johnngugi777@gmail.com>
2016-07-29 07:11:08 +0300

    Change info retreived

--- data-search/exercise.py
+++ data-search/exercise.py
@@ -4,7 +4,7 @@ with open ("MOCK_DATA.csv", "r") as csvfile:
     read_data = csv.reader(csvfile, delimiter=',')
     for row in read_data:
         if idnumber == row[0]:
-            print row[1] + ' '+ row[3] + ' '+ row[5]
+            print row[1] + ' '+ row[2] + ' '+ row[3]
             break
         else:
             pass
