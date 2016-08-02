c63e733f84b6fec59480df5c9607b4ffc157fba0
johnngugi <johnngugi777@gmail.com>
2016-08-02 06:58:05 +0300

    Change code

--- tribonacci/tribonaccif.py
+++ tribonacci/tribonaccif.py
@@ -1,6 +1,6 @@
-def tribonacci(n):   # return Fibonacci series up to n
+def tribonacci(n):  # return Fibonacci series up to n
     result = []
-    a, b,c = 0,0, 1
+    a, b, c = 0, 0, 1
     while b < n:
         result.append(b)
         a, b, c = b, c, a + b + c
