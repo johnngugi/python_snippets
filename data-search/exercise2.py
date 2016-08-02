b64a3ec7f9f48be308dceb8ba6d77afd2b7886b8
johnngugi <johnngugi777@gmail.com>
2016-08-02 06:57:25 +0300

    Change code

--- data-search/exercise2.py
+++ data-search/exercise2.py
@@ -1,8 +1,8 @@
 name = open("john.txt", "a")
-newname = str(raw_input("What is your name?"))
+newname = str(input("What is your name?"))
 name.read()
 if name.find(newname):
-    print "Error"
+    print("Error")
 else:
     name.write(newname + "\n")
     name.close()
