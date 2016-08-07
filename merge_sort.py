5704e189b5878e7bebddfec0bc43b631f06f7b89
johnngugi <johnngugi777@gmail.com>
2016-08-07 23:04:55 +0300

    Comment out code

--- merge_sort.py
+++ merge_sort.py
@@ -1,21 +1,19 @@
 def mergesort(lists):
 
-    if len(lists) > 1:    
+    if len(lists) > 1:    # if list is only 1 element it is already sorted
         mid = len(lists)/2
-        left = lists[:mid]
+        left = lists[:mid]  # divides list into two
         right = lists[mid:]
         
-        mergesort(left)
+        mergesort(left)  # continues to divide list recursively
         mergesort(right)
         
-        #print left
-        #print right
         
-        a = 0
+        a = 0  # placeholder values
         b = 0
         c = 0
         
-        while a < len(left) and b < len(right):
+        while a < len(left) and b < len(right):  # compares both sides of separated list
         
             if left[a] < right[b]:
                 lists[c] = left[a]
@@ -25,12 +23,12 @@ def mergesort(lists):
                 b += 1
             c += 1
             
-        while a < len(left):
+        while a < len(left): # sorts left side
             lists[c] = left[a]
             a += 1
             c += 1
             
-        while b < len(right):
+        while b < len(right): # sorts right side
             lists[c] = right[b]
             b += 1
             c += 1
