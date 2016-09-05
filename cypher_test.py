680d8dcf64a0f1fb806f2fbf75a770873a5729b0
johnngugi <johnngugi777@gmail.com>
2016-09-05 08:56:50 +0300

    Test for caesar cypher

---
+++ cypher_test.py
@@ -0,0 +1,9 @@
+# test for caesar cypher
+
+from caesar_cypher import cypher
+import unittest
+
+
+class testCase(unittest.TestCase):
+    def test_correct_cypher(self):
+        self.assertEquals(cypher("abcd"), "DEFG")
