139a96a3fdd958ef53902245862bfb3bedac3f08
johnngugi <johnngugi777@gmail.com>
2016-10-23 22:48:42 +0300

    Add docstring to Class

--- stack/stack.py
+++ stack/stack.py
@@ -1,4 +1,6 @@
 class Stack:
+    """An implementation of a stack in python."""
+
     def __init__(self):
         self.items = []
 
@@ -12,7 +14,7 @@ class Stack:
         return self.items.append(item)
 
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
